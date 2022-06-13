f51ad6d80ce8fa0f876dcfdd5156436c5458e1f9
fyj <fengyanjie@extrotec.com>
2022-06-13 17:13:50 +0800

    v9版本：flag解析命令参数

--- client/client.go
+++ client/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 )
@@ -31,8 +32,21 @@ func NewClient(serverIp string, serverPort int) *Client {
 	return client
 }
 
+var ip string
+var port int
+
+//通过启动命令后面加-ip 127.0.0.1 -port 18888形式启动
+func init() {
+	//第一个值：设置变量，第二个值参数名称，第三个值：默认值，第四个值：help时的提示
+	flag.StringVar(&ip, "ip", "127.0.0.1", "设置ip地址（默认127.0.0.1）")
+	flag.IntVar(&port, "port", 18888, "设置端口（默认18888）")
+}
+
 func main() {
-	client := NewClient("127.0.0.1", 18888)
+	//命令行解析
+	flag.Parse()
+
+	client := NewClient(ip, port)
 	if client == nil {
 		fmt.Println(">>>>>>>链接服务器失败....")
 		return
